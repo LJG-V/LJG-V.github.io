86ffafd700e335ef6cc186665ac5b4df6ab62a9d
CDN <1308715622@qq.com>
2020-02-12 19:17:48 +0800

    Site updated: 2020-02-12 19:17:46

--- dist/music.js
+++ dist/music.js
@@ -3,22 +3,22 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-		{
-        name: "你的答案",
+	{
+         name: "你的答案",
         artist: '阿冗',
         url: 'https://music.163.com/song/media/outer/url?id=1400256289.mp3',
         cover: 'http://p1.music.126.net/OlX-4S4L0Hdkyy_DQ27zag==/109951164459621658.jpg?param=130y130',
       },
 	  {
-        name: '不找了',
-        artist: '郭旭',
-        url: 'https://music.163.com/song/media/outer/url?id=29850531.mp3',
-        cover: 'http://p1.music.126.net/o25ptaSygDb2rVdTpIKexw==/17793396672429120.jpg?param=130y130',
+        name: '这些民谣 - 一次听个够',
+        artist: '翁大涵',
+        url: 'http://www.ytmp3.cn/down/60222.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
       },
       {
         name: '你的酒馆对我打了烊',
         artist: '陈雪凝',
-        url: 'https://music.163.com/song/media/outer/url?id=1341964346.mp3'，
+        url: 'http://www.ytmp3.cn/down/59770.mp3',
         cover: 'http://p1.music.126.net/LiRR__0pJHSivqBHZzbMUw==/109951163816225567.jpg?param=130y130',
       },
 	  {
